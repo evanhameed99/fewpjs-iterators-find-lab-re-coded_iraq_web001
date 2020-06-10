862f0e270f5c32aded99e5590c8b942cc036af1f
Evan hameed <evanhameed9966@gmail.com>
2020-06-10 04:24:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(arr){
+
+  let winOrLose = arr.find(obj =>{
+    if ( obj.result ==='W'){
+      return obj.year;
+    }
+  })
+  return winOrLose ; 
+
+}
