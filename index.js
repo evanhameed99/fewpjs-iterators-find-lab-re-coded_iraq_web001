edc6f3f85f269667bc251d68d93cf5eecfe38eab
Evan hameed <evanhameed9966@gmail.com>
2020-06-10 04:34:58 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+
+function superbowlWin(arr) {
+  let wonOrLost = arr.find((obj) => {
+    return obj.result === "W";
+  });
+  return !!wonOrLost?wonOrLost.year : undefined;
+}
